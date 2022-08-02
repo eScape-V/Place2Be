891367f0c3101b92bfb185f4628a01a0a2e109f3
NiJimmy <jimmy.ni2022@campus-eni.fr>
2022-08-02 16:22:06 +0200

    Début de Configuration des détails et de la liste des 'sorties' avec la base de données

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,26 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+/*
+ *
+ */
+
     /**
      * @Route("/", name="main_home")
      */
-    public function home(): Response
-    {
-        return $this->render('main/home.html.twig');
+    public function list(SortieRepository $repo){
+
+        $sorties = $repo->findAll();
+
+        return $this-> render('main/home.html.twig', [
+            "sorties" => $sorties
+        ]);
     }
+    /*
+    /**
+     * @Route("/", name="main_home")
+     *
+    *public function home(): Response
+    *{
+    *    return $this->render('main/home.html.twig');
+    *}
+    */
 
 
     /**
-     * @Route("/sortie/12", name="sortie_show")
+     * @Route("/sortie/", name="sortie_show")
      */
-    public function show(): Response
+    public function show(SortieRepository $repo): Response
     {
-        return $this->render('sortie/show.html.twig');
+        $sortie = $repo->find(2);
+
+        return $this->render('sortie/show.html.twig', [
+        "sortie" => $sortie
+        ]);
     }
 }
